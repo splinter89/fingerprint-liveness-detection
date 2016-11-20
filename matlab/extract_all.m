91089c6c4f97f5ef3df4ed0eeac9a4376a0b9096
splinter <splinta89@gmail.com>
2016-11-20 04:49:27 -0800

    refactor matlab feature extraction

--- matlab/extract_all.m
+++ matlab/extract_all.m
@@ -1,126 +1,115 @@
-methodName = 'BSIF'; % BSIF,LPQ,WLD
-collectorName = 'Bio'; % Bio,Dig,Ita,Sag
+methodName = 'BSIF'; % BSIF LPQ WLD
+collectorName = 'DigPerson'; % Bio DigPerson Ita Sag
+train_dir = '..\data-livdet-2015\Training_augmented';
+test_dir = '..\data-livdet-2015\Testing_augmented';
 
-switch(methodName)
+switch (methodName)
     case 'BSIF'
-        cd BSIF;
-        % load filter
-        filename=['./texturefilters/ICAtextureFilters_9x9_12bit'];
-        load(filename, 'ICAtextureFilters');
-        
-        Value_Real_Training = zeros(1000,4096);
-        Value_Real_Testing = zeros(1000,4096);
-        Value_Spoof_Training = zeros(1000,4096);
-        Value_Spoof_Testing = zeros(1000,4096);
+        load('./ICAtextureFilters_9x9_12bit', 'ICAtextureFilters');
+        n_features = 4096;
     case 'LPQ'
-        cd LPQ;
-        Value_Real_Training = zeros(1000,256);
-        Value_Real_Testing = zeros(1000,256);
-        Value_Spoof_Training = zeros(1000,256);
-        Value_Spoof_Testing = zeros(1000,256);
+        n_features = 256;
     case 'WLD'
-        cd WLD
-        Value_Real_Training = zeros(1000,960);
-        Value_Real_Testing = zeros(1000,960);
-        Value_Spoof_Training = zeros(1000,960);
-        Value_Spoof_Testing = zeros(1000,960);
+        n_features = 960;
 end
+Value_Real_Training = zeros(1000, n_features);
+Value_Real_Testing = zeros(1000, n_features);
+Value_Spoof_Training = zeros(1000, n_features);
+Value_Spoof_Testing = zeros(1500, n_features);
 
-imgNum = 1;
-for i=1:200
-    for j = 1:5
-        % load image
-        if strcmp(collectorName,'Bio') == 1 ||strcmp(collectorName,'Ita') == 1
-            imgRealTrain = rgb2gray(imread(strcat('../TestData/BiometrikaTrain/','Live/',num2str(i),'_',num2str(j),'.png')));
-            imgRealTest = rgb2gray(imread(strcat('../TestData/BiometrikaTest/','Live/',num2str(i),'_',num2str(j),'.png')));
-        else
-            imgRealTrain = imread(strcat('../TestData/BiometrikaTrain/','Live/',num2str(i),'_',num2str(j),'.bmp'));
-            imgRealTest = imread(strcat('../TestData/BiometrikaTest/','Live/',num2str(i),'_',num2str(j),'.bmp'));
-        end
-        
-        % extract features
-        switch(methodName)
-            case 'BSIF'
-                histTrainReal = bsif(imgRealTrain ,ICAtextureFilters,'h');
-                histTestReal = bsif(imgRealTest,ICAtextureFilters,'h');
-            case 'LPQ'
-                histTrainReal = lpq(imgRealTrain,3,1,1,'h');
-                histTestReal = lpq(imgRealTest,3,1,1,'h');
-            case 'WLD'
-                histTrainReal = wld(imgRealTrain,3,8);
-                histTestReal = wld(imgRealTest,3,8);
-        end
-        % store the feature
-        Value_Real_Training(imgNum,:)=histTrainReal;
-        Value_Real_Testing(imgNum,:)=histTestReal;
-        imgNum=imgNum+1;
+dir = strcat(train_dir, '\Digital_Persona\Live\');
+files = ls(strcat(dir, '*.png'));
+for i = 1:length(files)
+    f = files(i, :);
+    img = imread(strcat(dir, f));
+    img = rgb2gray(img);
+
+    res = [];
+    switch (methodName)
+        case 'BSIF'
+            res = bsif(img, ICAtextureFilters, 'h');
+        case 'LPQ'
+            res = lpq(img, 3, 1, 1, 'h');
+        case 'WLD'
+            res = wld(img, 3, 8).';
     end
+    Value_Real_Training(i, :) = res;
 end
 
-if strcmp(collectorName,'Bio') == 1 ||strcmp(collectorName,'Ita') == 1
-    Spoof_Method = {'EcoFlex','Gelatin','Latex','Silgum','WoodGlue'};
-end
+dir = strcat(test_dir, '\Digital_Persona\Live\');
+files = ls(strcat(dir, '*.png'));
+for i = 1:length(files)
+    f = files(i, :);
+    img = imread(strcat(dir, f));
+    img = rgb2gray(img);
 
-imgNum = 1;
-for k=1:5
-    for i=1:20
-        for j = 1:5
-            % load image
-            if strcmp(collectorName,'Bio') == 1 ||strcmp(collectorName,'Ita') == 1
-                imgSpoofTrain=rgb2gray(imread(strcat('../TestData/BiometrikaTrain/','Spoof/',Spoof_Method{k},'/',num2str(i),' (',num2str(j),').png')));
-                imgSpoofTest=rgb2gray(imread(strcat('../TestData/BiometrikaTest/','Spoof/',Spoof_Method{k},'/',num2str(i),' (',num2str(j),').png')));
-            else
-                imgSpoofTrain=imread(strcat('../TestData/BiometrikaTrain/','Spoof/',Spoof_Method{k},'/',num2str(i),' (',num2str(j),').bmp'));
-                imgSpoofTest=imread(strcat('../TestData/BiometrikaTest/','Spoof/',Spoof_Method{k},'/',num2str(i),' (',num2str(j),').bmp'));
-            end
-            
-            % extract features
-            switch(methodName)
-                case 'BSIF'
-                    histTrainReal = bsif(imgSpoofTrain ,ICAtextureFilters,'h');
-                    histTestReal = bsif(imgSpoofTest,ICAtextureFilters,'h');
-                case 'LPQ'
-                    histTrainReal = lpq(imgSpoofTrain,3,1,1,'h');
-                    histTestReal = lpq(imgSpoofTest,3,1,1,'h');
-                case 'WLD'
-                    histTrainReal=WLD_new(imgSpoofTrain,3,8);
-                    histTestReal=WLD_new(imgSpoofTest,3,8);
-            end
-            % store the feature
-            Value_Spoof_Training(imgNum,:)=histTrainReal;
-            Value_Spoof_Testing(imgNum,:)=histTestReal;
-            imgNum=imgNum+1;
-            
-        end
+    res = [];
+    switch (methodName)
+        case 'BSIF'
+            res = bsif(img, ICAtextureFilters, 'h');
+        case 'LPQ'
+            res = lpq(img, 3, 1, 1, 'h');
+        case 'WLD'
+            res = wld(img, 3, 8);
     end
+    Value_Real_Testing(i, :) = res;
 end
 
-cd ..
-nameTrainReal = strcat(methodName,'_7_12_motion_','Train_Real_',collectorName);
-eval([nameTrainReal,'=Value_Real_Training;']);
-save(strcat('./',methodName,'/',methodName,'_7_12_motion_','Train_Real_',collectorName),nameTrainReal);
+dir = strcat(train_dir, '\Digital_Persona\Fake\');
+files = ls(strcat(dir, '*.png'));
+for i = 1:length(files)
+    f = files(i, :);
+    img = imread(strcat(dir, f));
+    img = rgb2gray(img);
 
-nameTestReal = strcat(methodName,'_7_12_motion_','Test_Real_',collectorName);
-eval([nameTestReal,'=Value_Real_Testing;']);
-save(strcat('./',methodName,'/',methodName,'_7_12_motion_','Test_Real_',collectorName),nameTrainReal);
+    res = [];
+    switch (methodName)
+        case 'BSIF'
+            res = bsif(img, ICAtextureFilters, 'h');
+        case 'LPQ'
+            res = lpq(img, 3, 1, 1, 'h');
+        case 'WLD'
+            res = wld(img, 3, 8);
+    end
+    Value_Spoof_Training(i, :) = res;
+end
 
-nameTrainSpoof = strcat(methodName,'_7_12_motion_','Train_Spoof_',collectorName);
-eval([nameTrainSpoof,'=Value_Spoof_Training;']);
-save(strcat('./',methodName,'/',methodName,'_7_12_motion_','Train_Spoof_',collectorName),nameTrainReal);
+dir = strcat(test_dir, '\Digital_Persona\Fake\');
+files = ls(strcat(dir, '*.png'));
+for i = 1:length(files)
+    f = files(i, :);
+    img = imread(strcat(dir, f));
+    img = rgb2gray(img);
 
-nameTestSpoof = strcat(methodName,'_7_12_motion_','Test_Spoof_',collectorName);
-eval([nameTestSpoof,'=Value_Spoof_Testing;']);
-save(strcat('./',methodName,'/',methodName,'_7_12_motion_','Test_Spoof_',collectorName),nameTrainReal);
+    res = [];
+    switch (methodName)
+        case 'BSIF'
+            res = bsif(img, ICAtextureFilters, 'h');
+        case 'LPQ'
+            res = lpq(img, 3, 1, 1, 'h');
+        case 'WLD'
+            res = wld(img, 3, 8);
+    end
+    Value_Spoof_Testing(i, :) = res;
+end
 
-training_data = [Value_Real_Training;Value_Spoof_Training];
-testing_data = [Value_Real_Testing;Value_Spoof_Testing];
-real_label = ones(1000,1);
-fake_label = zeros(1000,1);
-training_label = [real_label;fake_label];
-testing_label = training_label;
-SVM_model = svmtrain(training_data,training_label);
-Predict_Real = svmclassify(SVM_model,Value_Real_Testing);
-correct_Real = sum(Predict_Real);
-Predict_Spoof = svmclassify(SVM_model,Value_Spoof_Testing);
-correct_Spoof = 1000-sum(Predict_Spoof);
-acc = (correct_Real + correct_Spoof) / 2000
+nameTrainReal = strcat(methodName, '_7_12_motion_', 'Train_Real_', collectorName);
+eval([nameTrainReal, '=Value_Real_Training;']);
+save(strcat('./', methodName, '_7_12_motion_', 'Train_Real_', collectorName), nameTrainReal);
+
+nameTestReal = strcat(methodName, '_7_12_motion_', 'Test_Real_', collectorName);
+eval([nameTestReal, '=Value_Real_Testing;']);
+save(strcat('./', methodName, '_7_12_motion_', 'Test_Real_', collectorName), nameTestReal);
+
+nameTrainSpoof = strcat(methodName, '_7_12_motion_', 'Train_Spoof_', collectorName);
+eval([nameTrainSpoof, '=Value_Spoof_Training;']);
+save(strcat('./', methodName, '_7_12_motion_', 'Train_Spoof_', collectorName), nameTrainSpoof);
+
+nameTestSpoof = strcat(methodName, '_7_12_motion_', 'Test_Spoof_', collectorName);
+eval([nameTestSpoof, '=Value_Spoof_Testing;']);
+save(strcat('./', methodName, '_7_12_motion_', 'Test_Spoof_', collectorName), nameTestSpoof);
+
+dlmwrite(strcat('./', nameTrainReal, '.txt'), Value_Real_Training, 'delimiter', '\t', 'newline', 'pc');
+dlmwrite(strcat('./', nameTestReal, '.txt'), Value_Real_Testing, 'delimiter', '\t', 'newline', 'pc');
+dlmwrite(strcat('./', nameTrainSpoof, '.txt'), Value_Spoof_Training, 'delimiter', '\t', 'newline', 'pc');
+dlmwrite(strcat('./', nameTestSpoof, '.txt'), Value_Spoof_Testing, 'delimiter', '\t', 'newline', 'pc');
